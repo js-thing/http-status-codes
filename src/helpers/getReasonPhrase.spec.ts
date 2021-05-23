1f21f24f01149a759721e4261f4748ae858e2928
Jnyan Ankur Kakati <jnyan.kakati@outlook.com>
2021-05-23 16:24:02 +0530

    added new helper function: getStatusCode

--- src/helpers/getReasonPhrase.spec.ts
+++ src/helpers/getReasonPhrase.spec.ts
@@ -5,9 +5,15 @@ import { HttpReasonPhrases } from '../HttpReasonPhrases';
 
 describe('getReasonPhrase', function () {
     it('# Returns the corresponding reason phrase for a valid defined status code', function () {
-        expect(getReasonPhrase(HttpStatusCodes.Ok)).to.equal(
-            HttpReasonPhrases.Ok
-        );
+        Object.entries(HttpStatusCodes).forEach((statusCodeKeyVal) => {
+            if (typeof statusCodeKeyVal[1] === 'number') {
+                expect(getReasonPhrase(statusCodeKeyVal[1])).to.equal(
+                    (HttpReasonPhrases as { [key: string]: string })[
+                        statusCodeKeyVal[0]
+                    ]
+                );
+            }
+        });
     });
     it('# Returns undefined for non-existent status code', function () {
         expect(getReasonPhrase(579)).to.equal(undefined);
